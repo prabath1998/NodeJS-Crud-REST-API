97749d14b6d2734d8aad3e93483788b450fa570d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-10 22:27:53 +0530

    changes in get api

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "john",
+    lastName: "doe",
+    age: 25,
+  },
+];
+
 //all routes in here are starting with /users
-router.get('/',(req,res)=>{
-    res.send('Hello');
+router.get("/", (req, res) => {
+  res.send(users);
 });
 
 export default router;

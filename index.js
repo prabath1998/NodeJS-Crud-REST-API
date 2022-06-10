83938f9d039f3df01419a6cddbfd23db080e1fec
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-10 22:22:42 +0530

    first get api

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    console.log("Test");
-    res.send('Hello from homepage');
-})
+app.use('/users',userRoutes);
 
-app.listen(PORT, ()=> console.log(`Server running on port : http://localhost:${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Hello from homepage");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on port : http://localhost:${PORT}`)
+);

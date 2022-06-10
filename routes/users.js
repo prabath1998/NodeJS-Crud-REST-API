65ef9dd68f3023df19b4e5bd598ea952b883d3be
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-10 22:40:15 +0530

    add user post api

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,22 @@ const users = [
     lastName: "doe",
     age: 25,
   },
+  {
+    firstName: "prabath",
+    lastName: "udayanga",
+    age: 23,
+  },
 ];
 
 //all routes in here are starting with /users
 router.get("/", (req, res) => {
   res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(`User with the name ${user.firstName} added successfully..!`);
 });
 
 export default router;

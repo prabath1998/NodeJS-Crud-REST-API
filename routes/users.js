5f6aa39cda9410f91a72938aae2a175979b9256e
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-11 08:34:18 +0530

    get user by id api

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,27 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "john",
-    lastName: "doe",
-    age: 25,
-  },
-  {
-    firstName: "prabath",
-    lastName: "udayanga",
-    age: 23,
-  },
-];
+const users = [];
 
 //all routes in here are starting with /users
 router.get("/", (req, res) => {
   res.send(users);
 });
 
+//add users
 router.post("/", (req, res) => {
   const user = req.body;
-  uuidv4();
-  users.push(user);
+
+  users.push({ ...user, id: uuidv4() });
   res.send(`User with the name ${user.firstName} added successfully..!`);
+});
+
+router.get("/:id", (req, res) => {
+  const id =req.params.id;
+  const foundUser = users.find((user)=> user.id === id);
+  res.send(foundUser);
 });
 
 export default router;

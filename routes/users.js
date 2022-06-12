1e64e294291dda58c0eff2e01b31178d1c037954
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-12 18:15:01 +0530

    update user api

--- routes/users.js
+++ routes/users.js
@@ -19,16 +19,38 @@ router.post("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const id =req.params.id;
-  const foundUser = users.find((user)=> user.id === id);
+  const id = req.params.id;
+  const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
 });
 
-router.delete('/:id',(req,res)=>{
+router.delete("/:id", (req, res) => {
   const id = req.params.id;
 
-  users = users.filter((user)=> user.id !== id);
-  res.send(`User with id ${id} deleted from database.`)
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with id ${id} deleted from database.`);
+});
+
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id === id);
+
+
+  if(firstName){
+    user.firstName = firstName;
+  }
+
+  if(lastName){
+    user.lastName = lastName;
+  }
+
+  if(age){
+    user.age = age;
+  }
+
+  res.send('User updated successfully')
 });
 
 export default router;
